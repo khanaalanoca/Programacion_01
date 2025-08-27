6f1519d8c134fcaa5c799acebf70bd3d974b3f35
khanaalanoca <khana.alanoca@ucb.edu.bo>
2025-08-26 22:46:17 -0400

    primer ejercicio

---
+++ PRACTICA_01/Ejercicio_01_01.cpp
@@ -0,0 +1,17 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Khana Brigida Alanoca Limachi
+// Carnet: 13760727 L.P.
+// Carrera del estudiante: Ingeniería Biomedica
+// Fecha creación: 26/08/2025
+// Número de ejercicio: 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Hola soy estudiate de programacion " << endl;
+    cout << " Khana Brigida Alanoca Limachi " << endl;
+    cout << " Ingenieria Biomedica " << endl;
+    return 0;
+}
